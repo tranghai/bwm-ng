3b8a9761aa205e79d6472220c2cc6fabe5670b64
Trang Hải <tranghai.khtn@outlook.com>
2018-11-02 16:53:05 +0700

    Adds register, adds login and auth middleware routes on Server

--- server/fake-db.js
+++ server/fake-db.js
@@ -1,5 +1,5 @@
 const Rental = require('./models/rental');
-
+const User = require('./models/user');
 class FakeDb {
     constructor() {
         this.rentals = [
@@ -42,22 +42,39 @@ class FakeDb {
                 shared: false,
                 //createAt: '24/12/2017'
             }];
+
+        this.users = [
+            {
+                username: 'Test User',
+                email: 'test@gmail.com',
+                password: 'testtest'
+            }
+        ]
     }
 
-   async cleanDb(){
+    async cleanDb() {
+        await User.remove({});
         await Rental.remove({});
     }
 
-    pushRentalsToDb() {
+    pushDataToDb() {
+
+        const user = new User(this.users[0]);
+
         this.rentals.forEach((rental) => {
             const newRental = new Rental(rental);
+            newRental.user = user;
+
+            user.rentals.push(newRental);
             newRental.save();
         });
+
+        user.save();
     }
 
-    seedDb(){
-        this.cleanDb();
-        this.pushRentalsToDb();
+    async seedDb() {
+        await this.cleanDb();
+        this.pushDataToDb();
     }
 }
 
