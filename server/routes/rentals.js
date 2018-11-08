c534dc6e3b669c5981895e6799e76f961230528c
Trang Hải <tranghai.khtn@outlook.com>
2018-11-08 15:37:25 +0700

    Adds manage rental and booking endpoint and add delete rental endpoint

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -9,6 +9,20 @@ router.get('/secret', UserCtrl.authMiddleware, function (req, res) {
     res.json({ 'secret': true });
 })
 
+router.get('/manage', UserCtrl.authMiddleware, function(req, res){
+    const user = res.locals.user;
+
+    Rental.where({user})
+        .populate('bookings')
+        .exec(function(err, foundRentals){
+            if(err){
+                return res.status(422).send({ errors : normalizeErrors(err.errors) });
+            }
+
+            return res.json(foundRentals);
+        });
+});
+
 router.get('/:id', function (req, res) {
     const rentalId = req.params.id;
 
@@ -58,5 +72,38 @@ router.post('', UserCtrl.authMiddleware, function (req, res) {
 
         return res.json(newRental);
     });
+});
+
+router.delete('/:id', UserCtrl.authMiddleware, function(req, res){
+    const user = res.locals.user;
+
+    Rental.findById(req.params.id)
+        .populate('user', '_id')
+        .populate({
+            path: 'bookings',
+            select: 'startAt',
+            match: { startAt: { $gt: new Date()}}
+          })
+        .exec(function(err, foundRental){
+            if(err){
+                return res.status(422).send({ errors : normalizeErrors(err.errors) });
+            }
+
+            if(user.id !== foundRental.user.id){
+                return res.status(422).send({ errors: [{ title: 'Invalid User!', detail: 'You are not rental owner!'}]});
+            }
+ 
+            if(foundRental.bookings.length > 0){
+                return res.status(422).send({ errors: [{ title: 'Active Bookings!', detail: 'Cannot delete rental with active bookings!'}]});
+            }
+
+            foundRental.remove(function(err){
+                if(err){
+                    return res.status(422).send({ errors : normalizeErrors(err.errors) });
+                }
+
+                return res.json({ 'status' : 'deleted'});
+            });
+        })
 });
 module.exports = router;
